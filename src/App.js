87ea2e67d5fb17d3b2291f31bd0e7b666b670a94
sonofglassperson <ntshabalalait@gmail.com>
2024-10-05 12:24:31 +0200

    Test env variables

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ function App() {
     <div className="App">
       <header clas sName="App-header">
             <h1>
-                First React app
+                First React app: Different environments
             </h1>
+            <h2>
+                {process.env.REACT_APP_NODE_ENV}
+            </h2>
+            <h2>
+                ENV: {process.env.REACT_APP_NODE_ENV}
+            </h2>
+            
       </header>
     </div>
   );
